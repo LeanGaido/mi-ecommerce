24b239f538020198fa9bd63a9aeb6102bd5d8bc3
Leandro Gaido <lgaido@compragamer.net>
2022-06-08 21:33:45 -0300

    Holaaaaa

--- src/App.js
+++ src/App.js
@@ -1,74 +1,31 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-// import ComponenteTest from './components/Tareas/ComponenteTest';
-// import TestPromesas from './components/Tareas/TestPromesas';
-//import TestFetchAPI from './components/Tareas/TestFetchAPI';
-import { useEffect, useState } from 'react';
-import { Container, Spinner } from 'react-bootstrap';
+import Home from './components/Home/Home';
+import Ofertas from './components/Ofertas/Ofertas';
+import Productos from './components/Productos/Productos';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [result, setResult] = useState([]);
   const [cantCarrito, setCantCarrito] = useState(0);
-
-  const getData = async() => {
-    try {
-      const data = await fetch("/Data/Productos.json")
-      const res = new Promise((respuesta, rechazo) => {
-          setTimeout(() => {
-            respuesta(data.json());
-          }, 3000)
-      });
-
-      res.then((respuesta) => {
-          console.log(respuesta);
-          setResult(respuesta);
-      }).catch((error) => {
-          setError(true);
-      }).finally(() => {
-          setLoading(false);
-      })
-
-    } catch (error) {
-      
-    }
-  }
-
-  useEffect(() => {
-    getData();
-  }, [])
 
   return (
     <div className="App">
-      <NavBar cantCarrito={cantCarrito} />
+      <BrowserRouter>
+        <NavBar cantCarrito={cantCarrito} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Ofertas" element={<Ofertas />} />
+          <Route path="/Productos" element={<Productos setCantCarrito={setCantCarrito} />} />
+        </Routes>
+      </BrowserRouter>
       {/*
         <ComponenteTest />
         <TestPromesas />
         <TestFetchAPI />
       */}
-      <Container>
-            {
-                loading ?
-                <Container>
-                  <br/>
-                  <br/>
-                  <Spinner animation="border" role="status" variant="info">
-                    <span className="visually-hidden">Loading...</span>
-                  </Spinner>
-                </Container> :
-                <div>
-                    {
-                        error ?
-                        "There was an error inthe Payment" :
-                        <ItemListContainer grettings={result} 
-                                           setCantCarrito={setCantCarrito} />
-                    }
-                </div>
-            }
-        </Container>
-      
+
       {/* <header className="App-header">
         <img src="/logo.png" className="App-logo" alt="logo" />
         <p>
